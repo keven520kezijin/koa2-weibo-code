62f43c44e426db697039df9743c702c800a290c4
keven518 <957314724@qq.com>
2021-01-28 22:20:30 +0800

    feat: 登录接口

--- readme.md
+++ readme.md
@@ -2,3 +2,6 @@ refactor: 重构
 feat: 新增加
 feature-xxx: 增加功能的分支
 fix-xxx:解决bug
+
+redis-server.exe redis.windows.conf
+redis-cli
--- routes/api/user.js
+++ routes/api/user.js
@@ -4,12 +4,14 @@
  */
 
 const router = require('koa-router')()
-const { isExist, register } = require('../../controller/user')
+const { isExist, register, login } = require('../../controller/user')
+const userValidate = require('../../validator/user')
+const { genValidator } = require('../../middlewares/validator')
 
 router.prefix('/api/user')
 
 // 注册路由
-router.post('/register', async (ctx, next) => {
+router.post('/register', genValidator(userValidate), async (ctx, next) => {
   const { userName, password, gender } = ctx.request.body
   // 调用 controller， 返回
   ctx.body = await register({
@@ -24,6 +26,13 @@ router.post('/isExist', async (ctx, next) => {
   const { userName } = ctx.request.body
   ctx.body = await isExist(userName)
 
+})
+
+// 登录
+router.post('/login', async(ctx, next) => {
+  const { userName, password } = ctx.request.body
+  // controller
+  ctx.body = await login(ctx, userName, password)
 })
 
 module.exports = router
