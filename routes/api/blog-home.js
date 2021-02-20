ddb8e49d514f2ca75ae5aaa5e1b735283571db2f
keven518 <957314724@qq.com>
2021-02-20 15:39:03 +0800

    feat: 创建微博 xss 过滤和格式校验

--- routes/api/blog-home.js
+++ routes/api/blog-home.js
@@ -3,19 +3,21 @@
  * @author keven
  */
 
- const router = require('koa-router')()
- const { loginCheck } = require('../../middlewares/loginChecks')
- const { create } = require('../../controller/blog-home')
+const router = require('koa-router')()
+const { loginCheck } = require('../../middlewares/loginChecks')
+const { create } = require('../../controller/blog-home')
+const { genValidator } = require('../../middlewares/validator')
+const blogValidate = require('../../validator/blog')
 
- router.prefix('/api/blog')
+router.prefix('/api/blog')
 
- // 创建微博
- router.post('/create', loginCheck, async (ctx, next) => {
-   const { content, image } = ctx.request.body
-   const { id: userId } = ctx.session.userInfo
-   // controller
-   ctx.body = create({ userId, content, image })
- })
+// 创建微博
+router.post('/create', loginCheck, genValidator(blogValidate), async (ctx, next) => {
+  const { content, image } = ctx.request.body
+  const { id: userId } = ctx.session.userInfo
+  // controller
+  ctx.body = create({ userId, content, image })
+})
 
- module.exports = router
+module.exports = router
 
